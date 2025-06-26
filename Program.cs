381b3f8477785859db7888b20aa4bffacfc7b01c
3jamees <jamesiiking8@gmail.com>
2025-06-25 21:29:16 -0400

    Greeting and description of game

--- Program.cs
+++ Program.cs
@@ -2,4 +2,6 @@
 // Name: James King II
 // Date: June 25, 2025
 // Lab 5 - Madlibs#1
+Console.WriteLine("Welcome to Mad Libs!");
+Console.WriteLine("You'll be asked for different types of words to complete a funny vacation story.");
 
